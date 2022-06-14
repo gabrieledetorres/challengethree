955389f3c326c7aaaa0d4dbdf426af38368c75e3
gabrieletorres <secgabriele@gmail.com>
2022-06-14 15:41:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+/* 
+    for(let count=1; count<=10;count++){
+        console.log(count);
+} 
+*/
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+/* let count = 0;
+
+    while(count<=10){
+        console.log(count);
+        count++;
+}
+*/
+    
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+/*
+    for (let count = 1; count <=100; count +=2){
+        console.log(count)
+} 
+*/
+
+/*
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+    for(let count=0; count<=100;count+=2){
+        console.log(count);
+} 
+*/
